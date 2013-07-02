a45a878bcc10278f2303be50284e374bdbe1857a
Andrew Rothstein <andrew.rothstein@gmail.com>
2013-07-01 23:18:03 -0400

    fleshed out the CashFlows class

--- src/main/scala/CashFlows.scala
+++ src/main/scala/CashFlows.scala
@@ -1,14 +1,22 @@
 import date.DateCalc
 import date.Date
 
-case class CashFlows(slices :List[RealizedFlow])
 
-object CashFlows {
+case class CashFlows(slices :List[RealizedFlow]) {
+  private def sumFolder(sum :Double, s :Double) = sum + s
+  private def sum(v :List[Double]) = (0.0 /: v)(sumFolder)
 
-  def wal(cf: CashFlows, start: Date, dc: DateCalc) = {
-    val ts = cf.slices.map((rf) => dc.alpha(start, rf.payDay.actualPayDay))
-    val sumPrin = cf.slices.foldRight(0.0)((rf, s) => s + rf.principal)
-    val printimest = ts.zip(cf.slices)
+  lazy val principals = slices.map((s => s.principal))
+  lazy val sumPrincipal = sum(principals)
+  lazy val interests = slices.map((s => s.interest))
+  lazy val sumInterest = sum(interests)
+
+  def times(start :Date, dc :DateCalc) = slices map (s => dc.alpha(start, s.payDay.actualPayDay))
+  
+  def wal(start: Date, dc: DateCalc) = {
+    val timeWeightedPrincipal = (times(start, dc) zip principals) map (t => t._1 * t._2)
+    val sumTtimeP = sum(timeWeightedPrincipal)
+    sumTtimeP / sumPrincipal
   }
   
   
