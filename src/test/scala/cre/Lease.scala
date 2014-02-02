96abff26d73b73ae9d0b57d4267022e9beca588a
Andrew Rothstein <andrew.rothstein@gmail.com>
2014-02-01 21:03:44 -0500

    fix typos

--- src/test/scala/cre/Lease.scala
+++ src/test/scala/cre/Lease.scala
@@ -3,9 +3,10 @@ package cre
 case class Lease(remTerm :Int, rentPerSqFt :Double)
 
 object Lease {
+
   lazy val Vacant = Lease(0,0)
   
-  def next(l :Lease, renewFunc :Func[Lease) => :Lease) = {
-    if (l.remTerm == 0) renewFunc(l) else Lease(remTerm - 1, rentPerSqFt)
+  def next(l :Lease, renewFunc :Function[Lease, Lease]) = {
+    if (l.remTerm == 0) renewFunc(l) else Lease(l.remTerm - 1, l.rentPerSqFt)
   }
 }
